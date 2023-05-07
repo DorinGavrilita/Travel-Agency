720e344bba989793afd7c02b87b14595c16292aa
Dorin Gavrilita <gavrilitadorin5@gmail.com>
2023-05-07 23:36:25 +0300

    contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from './Components/Header';
 import { HelpTravels } from './Components/HelpTravels';
 import { Gallery } from './Components/Gallery';
+import { Contact } from './Components/Contact';
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
       <Header />
       <HelpTravels />
       <Gallery />
+      <Contact />
     </>
   )
 }
---
+++ src/Components/Contact.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export const Contact = () => {
+  return (
+    <div className="contact">
+      <Container>
+        <Row>
+          <Col sm={4}>
+            <div className="photo-card bg-white d-flex justify-content-center align-items-center position-relative">
+              <div className="contact-image" />
+              <div className="contact-image-mobile position-absolute" />
+            </div>
+          </Col>
+          <Col sm={8}>
+            <div className='w-100 h-100 bg-white contact-text'>
+              <p className='text-uppercase'>
+                CONVALLIS POSUERE MORBI LEO URNA MOLESTIE AT ELEMENTUM EU FACILISIS. VEHICULA IPSUM A ARCU CURSUS VITAE.
+              </p>
+              <div className="contacts mt-4 d-flex align-items-center">
+                <div className="phone-icon me-2" />
+                <h2>+1 (234) 567-8910</h2>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div >
+  )
+}
